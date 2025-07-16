2343c3bdf1c718fb3125f655f3cc8829db774ee4
ArthurDev <francasilvaarthur95@gmail.com>
2025-07-16 17:44:38 -0300

    trocando o banco de dados para o postgresql

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -16,6 +16,10 @@ from models import Usuario, Tarefa
 # access to the values within the .ini file in use.
 config = context.config
 
+DATABASE_URL = os.getenv("DATABASE_URL")
+if DATABASE_URL is not None:
+    config.set_main_option("sqlalchemy.url", DATABASE_URL)
+
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
--- backend/database.py
+++ backend/database.py
@@ -1,10 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
 
-DECLATARIVE_URL = 'sqlite:///db.db'
+load_dotenv()
 
-engine = create_engine(DECLATARIVE_URL)
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL)
 
 sessionlocal = sessionmaker(
     autocommit=False,
